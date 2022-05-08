40231625c8c45aee5141fb9f2b8132c5f2d7c282
Ignacio Tula <itula@logos.net.ar>
2022-05-08 18:56:46 -0300

    Update NodoTest.java

--- src/grafo/NodoTest.java
+++ src/grafo/NodoTest.java
@@ -23,6 +23,7 @@ public class NodoTest {
 
 	}
 
+	
 	@Test
 	public void testGetInformacion() {
 		assertTrue("0" == this.nodoA.getInformacion());
@@ -48,9 +49,87 @@ public class NodoTest {
 	}
 	
 	
-	@Test 
+	@Test
 	public void testNoSonVecinosAun() {
 		assertFalse(this.nodoA.esVecino(nodoB));
 	}
+	
+	
+
+	@Test
+	public void testAgregarVecino() {
+		this.nodoA.agregarVecino(nodoB, 1.0f);
+		Distancia<String> distanciaEsperada = new Distancia<String>(nodoB, 1.0f);
+		Distancia<String> distanciaReal = this.nodoA.obtenerDistancia(0);
+		assertTrue(distanciaEsperada.equals(distanciaReal));
+	}
+	
+	
+	@Test
+	public void testAgregarSegundoVecino() {
+		this.nodoA.agregarVecino(nodoB, 1.0f);
+		this.nodoA.agregarVecino(nodoC, 2.0f);
+		Distancia<String> distanciaEsperada = new Distancia<String>(nodoC, 2.0f);
+		Distancia<String> distanciaReal = this.nodoA.obtenerDistancia(1);
+		assertTrue(distanciaEsperada.equals(distanciaReal));
+	}
+	
+	@Test
+	public void testVecinosDiferentes() {
+		this.nodoA.agregarVecino(nodoB, 1.0f);
+		this.nodoA.agregarVecino(nodoC, 2.0f);
+		Distancia<String> distanciaEsperada = this.nodoA.obtenerDistancia(0);
+		Distancia<String> distanciaReal = this.nodoA.obtenerDistancia(1);
+		assertFalse(distanciaEsperada.equals(distanciaReal));
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testNoSepuedeSerDosVecesVecinos() {
+		this.nodoA.agregarVecino(nodoB, 1.0f);
+		this.nodoA.agregarVecino(nodoB, 2.0f);
+		
+	}
+	
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testNoEliminarvecinoQueNoEsVecino() {
+		this.nodoA.eliminarVecino(nodoB);
+		
+		
+	}
+	
+
+	@Test (expected = IllegalArgumentException.class)
+	public void testNoAgregarVecinosDuplicados() {
+		this.nodoA.agregarVecino(nodoB, 1.0f);
+		this.nodoA.agregarVecino(nodoC, 2.0f);
+		this.nodoA.agregarVecino(nodoB, 1.0f);
+		
+	}
+	
+	@Test 
+	public void testObtenerBuenIndice() {
+		this.nodoA.agregarVecino(nodoB, 1.0f);
+		this.nodoA.agregarVecino(nodoC, 2.0f);
+		
+		int indice = this.nodoA.indiceDeVecino(nodoC);
+		
+		assertEquals(indice,1);
+		
+	}
+	
+	@Test 
+	public void testObtenerBuenaCantidadDeVecinos() {
+		this.nodoA.agregarVecino(nodoB, 1.0f);
+		this.nodoA.agregarVecino(nodoC, 2.0f);
+		
+		int cant = this.nodoA.obtenerCantidadDeVecinos();
+		
+		assertEquals(cant,2);
+		
+	}
+	
+	
+	
 
 }
